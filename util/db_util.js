3719e2e347c4d10eef4bd2c0c02d1038a1076cc5
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-13 11:13:15 +0530

    adding missed requirements for sqlite3 package

--- util/db_util.js
+++ util/db_util.js
@@ -1,5 +1,6 @@
 
 const path = require('path');
+const sqlite3 = require('sqlite3').verbose();
 // write function get database for given file name which will create file at data folder , add suffix of .db 
 // and return the database object
 function getDatabase(filename) {
