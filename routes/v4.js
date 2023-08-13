7e5c96b506a4d39c4ba8c10516964508babe0032
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-13 19:44:24 +0530

    add column,  delete row, update row, drop table

--- routes/v4.js
+++ routes/v4.js
@@ -93,4 +93,127 @@ router.get('/files/:filename/tables/:table_name/rows', (req, res) => {
     });
 });
 
+// delete row 
+router.delete('/files/:filename/tables/:table_name/rows/:row_id', (req, res) => {
+  const filename = req.params.filename;
+  const db  = getDatabase(filename);
+  const tableName = req.params.table_name;
+  const rowId = req.params.row_id;
+
+  const query = `DELETE FROM "${tableName}" WHERE id = ?`;
+
+  db.run(query, [rowId], function(err) {
+      db.close();
+      if (err) {
+          console.error('Error:', err.message);
+          return res.status(500).json({ error: err.message });
+      }
+      if (this.changes === 0) {
+          return res.status(404).json({ error: 'Row not found' });
+      }
+      res.json({ message: 'Row deleted successfully' });
+  });
+});
+
+// update row
+router.put('/files/:filename/tables/:table_name/rows/:row_id', (req, res) => {
+  const filename = req.params.filename;
+  const db  = getDatabase(filename);
+  const tableName = req.params.table_name;
+  const rowId = req.params.row_id;
+  const { columns } = req.body;
+
+  if (!Array.isArray(columns)) {
+      return res.status(400).json({ error: "Columns should be an array." });
+  }
+
+  const updateColumns = columns.map(col => `"${col.name}" = ?`).join(', ');
+  const values = columns.map(col => col.value);
+
+  const query = `UPDATE "${tableName}" SET ${updateColumns} WHERE id = ?`;
+
+  values.push(rowId);
+
+  db.run(query, values, function(err) {
+      db.close();
+      if (err) {
+          console.error('Error:', err.message);
+          return res.status(500).json({ error: err.message });
+      }
+      if (this.changes === 0) {
+          return res.status(404).json({ error: 'Row not found' });
+      }
+      res.json({ message: 'Row updated successfully' });
+  });
+});
+
+// select row by condition column, value
+router.get('/files/:filename/tables/:table_name/rows', (req, res) => {
+  const filename = req.params.filename;
+  const db  = getDatabase(filename);
+  const tableName = req.params.table_name;
+  const { conditionColumn, conditionValue } = req.query;
+
+  let query = `SELECT * FROM "${tableName}"`;
+
+  if (conditionColumn && conditionValue) {
+      query += ` WHERE "${conditionColumn}" = ?`;
+  }
+
+  db.all(query, [conditionValue], (err, rows) => {
+      db.close();
+      if (err) {
+          console.error('Error:', err.message);
+          return res.status(500).json({ error: err.message });
+      }
+      res.json(rows);
+  });
+});
+
+// Drop Table API
+router.delete('/files/:filename/tables/:table_name', (req, res) => {
+  const filename = req.params.filename;
+  const db  = getDatabase(filename);
+  const tableName = req.params.table_name;
+
+  const query = `DROP TABLE IF EXISTS "${tableName}"`;
+
+  db.run(query, function(err) {
+      db.close();    
+      if (err) {
+          console.error('Error:', err.message);
+          return res.status(500).json({ error: err.message });
+      }
+      res.json({ message: 'Table dropped successfully' });
+  });
+});
+
+// Add Column to Table API
+router.post('/files/:filename/tables/:table_name/columns', (req, res) => {
+  const filename = req.params.filename;
+  const db  = getDatabase(filename);
+  const tableName = req.params.table_name;
+  const { name, type, defaultValue } = req.body;
+
+  let query = `ALTER TABLE "${tableName}" ADD COLUMN "${name}" ${type}`;
+
+  if (defaultValue !== undefined) {
+      if (type.toUpperCase() === 'TEXT') {
+          // If the data type is TEXT, add single quotes around the default value
+          query += ` DEFAULT '${defaultValue}'`;
+      } else {
+          query += ` DEFAULT ${defaultValue}`;
+      }
+  }
+
+  db.run(query, function(err) {
+      db.close();
+      if (err) {
+          console.error('Error:', err.message);
+          return res.status(500).json({ error: err.message });
+      }
+      res.json({ message: 'Column added successfully' });
+  });
+});
+
 module.exports = router;
