17bbd90526cf1bdd8ffcfff59a3ffef762fb3f0e
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2023-08-13 10:41:58 +0530

    v4 code changes

---
+++ util/db_util.js
@@ -0,0 +1,12 @@
+
+// write function get database for given file name which will create file at data folder , add suffix of .db 
+// and return the database object
+function getDatabase(filename) {
+    const dbPath = path.join(__dirname, '..', 'data', `${filename}.db`);
+    const db = new sqlite3.Database(dbPath);
+    return db;
+}
+
+module.exports = {
+    getDatabase
+}
